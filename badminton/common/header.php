cf47250068c4a6e5c8d5889b3208727ead32878d
GalaevDmitrii <115530672+GalaevDmitrii@users.noreply.github.com>
2023-01-19 11:13:23 +0100

    Delete badminton directory

--- badminton/common/header.php
+++
@@ -1,6 +0,0 @@
-<header>
-    <h1 class="site-heading text-center text-faded d-none d-lg-block">
-        <span class="site-heading-upper text-primary mb-3">Marine life</span>
-        <span class="site-heading-lower">What is the secret of giant octopus</span>
-    </h1>
-</header>
